dda42741c366b0fe48dc61ce277450f1fc0338de
LesiukO <lesuksasha@gmail.com>
2019-12-14 14:01:25 +0200

    Reading input data

--- app.js
+++ app.js
@@ -6,16 +6,39 @@ var budgetController = (function () {
 
 
 var UIController = (function () {
-    
-    // some code
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    }
+
+    return {
+        getInput: function () {
+            return {
+                type : document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings
+        }
+    }
     
 }) ()
 
 
 var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMstrings()
     
     var ctrlAddItem = function () {
         // 1. Get input data
+        var input = UICtrl.getInput()
+        console.log(input)
 
         // 2. Add the item to the budget controller
 
@@ -24,11 +47,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-
-        console.log('its alive')
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
     document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
